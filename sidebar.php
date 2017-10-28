9c10fbdb0aa015e3e60cc2529d1ed4f60b62f10c
bad4iz <bad4iz@gmail.com>
2017-10-28 17:18:36 +0400

    меню запрос

--- sidebar.php
+++ sidebar.php
@@ -63,12 +63,12 @@
 <h3 class="toc-title">Запрос</h3>
 <ul class="toc-links">
   <li><a href="/objects/request">Обзор</a></li>
-  <li><a href="/objects/request#the-request-method">Метод</a></li>
-  <li><a href="/objects/request#the-request-headers">Заголовки</a></li>
-  <li><a href="/objects/request#the-request-body">Тело</a></li>
+  <li><a href="/objects/request#the-request-method">Метод запроса</a></li>
+  <li><a href="/objects/request#the-request-headers">Заголовки запросов</a></li>
+  <li><a href="/objects/request#the-request-body">Тело запроса</a></li>
   <li><a href="/objects/request#uploaded-files">Загруженные файлы</a></li>
   <li><a href="/objects/request#request-helpers">Помощьники</a></li>
-  <li><a href="/objects/request#route-object">Обьект маршрута</a></li>
+  <li><a href="/objects/request#route-object">Обьект роутера</a></li>
   <li><a href="/objects/request#media-type-parsers">Анализаторы типов мультимедиа</a></li>
 </ul>
 
